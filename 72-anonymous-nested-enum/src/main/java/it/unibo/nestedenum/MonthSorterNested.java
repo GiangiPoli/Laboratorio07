9b194b4a04a96f5e557be5a070e1bdb6c1232a01
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2025-11-08 18:17:20 +0100

    fromString() method implemented and exercise 72 done

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,7 +1,8 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
+//import java.util.Locale;
+//import java.util.Locale;
 import java.util.Objects;
 
 /**
@@ -9,20 +10,35 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
-    //Class Enumeration
-    public enum Month{
-        GENNAIO(31),
-        FEBBRAIO(28),
-        MARZO(31),
-        APRILE(30),
-        MAGGIO(31),
-        GIUGNO(30),
-        LUGLIO(31),
-        AGOSTO(31),
-        SETTEMBRE(30),
-        OTTOBRE(31),
-        NOVEMBRE(30),
-        DICEMBRE(31);
+    //Class Fields
+    private Comparator<String> ByDays = new SortByDate();
+    private Comparator<String> ByMonth = new SortByMonthOrder();
+
+    //Class Method
+    @Override
+    public Comparator<String> sortByDays() {
+        return ByDays;
+    }
+
+    @Override
+    public Comparator<String> sortByOrder() {
+        return ByMonth;
+    }
+
+    //Class Nest
+    private enum Month{
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
         
         //Class Fields
         private int days;
@@ -33,18 +49,61 @@ public final class MonthSorterNested implements MonthSorter {
         }
         
         //Class Method
-        Month fromString(String monthName) throws Exception {
-            return Month.valueOf(monthName);
+        static Month fromString(String monthName) {
+            
+            Objects.requireNonNull(monthName);
+
+            try {
+                return Month.valueOf(monthName);
+            } catch (final IllegalArgumentException e) {
+                
+                Month monthSearched = null;
+                int counter = 0;
+
+                for (final Month value  : Month.values()) {
+
+                    if (value.toString().toLowerCase().startsWith(monthName.toLowerCase())) {
+
+                        monthSearched = value;
+                        counter++;
+
+                    }
+                
+                }
+                if ( counter == 0 || counter > 1 ) {
+                    throw new IllegalArgumentException("No Month with such a name or too many with the specified letters"
+                    + "\n" 
+                    + counter 
+                    +" letter passed match with possibile Month name" );
+                }
+                
+                return monthSearched;
+            }
+                
+        }
+    }
+    
+    //Nested Class
+    private static class SortByMonthOrder implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            final Month first = Month.fromString(o1);
+            final Month second = Month.fromString(o2);
+            Objects.requireNonNull(first);
+            return first.compareTo(second);
+        }
+    }
+    
+    private static class SortByDate implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            final Month first = Month.fromString(o1);
+            final Month second = Month.fromString(o2);
+            Objects.requireNonNull(first);
+            return Integer.compare(first.days, second.days);
         }
     }
 
-    @Override
-    public Comparator<String> sortByDays() {
-        return null;
-    }
-
-    @Override
-    public Comparator<String> sortByOrder() {
-        return null;
-    }
 }
